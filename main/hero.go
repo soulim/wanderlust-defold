12e1c9c294b967eae659e8a9fe35c2bb4589adc7
Alexander Sulim <hello@sul.im>
2023-03-02 19:32:18 +0100

    Add hero controlled via arrow keys

---
+++ main/hero.go
@@ -0,0 +1,30 @@
+components {
+  id: "controller"
+  component: "/main/hero.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "sprite"
+  component: "/main/hero.sprite"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
